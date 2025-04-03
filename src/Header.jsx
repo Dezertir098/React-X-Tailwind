1687852d1c29f18a099422c4d93a19040d2bf8da
Dezertir098 <fazan7640@mail.ru>
2025-04-03 15:29:21 +0300

    Test

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 // import './header.css'; // Подключим стили
 // import Button from './button';
+import photo from './assets/react.svg';
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);  // Состояние меню
@@ -14,7 +15,7 @@ function Header() {
     <header className="fixed top-0 left-0 w-full bg-black text-white z-50" id="header">
   <div className="container mx-auto flex justify-between items-center py-4 px-6" style={{ maxWidth: '1200px' }}>
     {/* Логотип */}
-    <img className="logo h-12" src="./public\logo.svg" alt="Laf" />
+    <img className="logo h-12" src="src/assets/react.svg" alt="Laf" />
 
     {/* Меню */}
     <div className={`menu ${isMenuOpen ? 'open' : ''} hidden md:block`}>
