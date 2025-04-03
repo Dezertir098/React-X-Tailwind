ba29da7c73d6be5d53513ce8d5a4fbd12549fbb1
Dezertir098 <fazan7640@mail.ru>
2025-04-03 15:39:44 +0300

    Test

--- src/Fotter.jsx
+++ src/Fotter.jsx
@@ -11,32 +11,25 @@ function Fotter() {
   };
 
   return (
-    <footer className="bg-black text-white py-6 w-full" id="footer" style={{margin: '0 auto', position: 'absolute', bottom: '0', left: '50%', transform: 'translateX(-50%)' }}>
+    <footer className="bg-black text-white py-6 w-full" id="footer" style={{ margin: '0 auto', position: 'absolute', bottom: '0', left: '50%', transform: 'translateX(-50%)' }}>
   <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-6" style={{ maxWidth: '1200px' }}>
-    
-
     <div className="flex items-center space-x-6 w-full" style={{ maxWidth: '1200px' }}>
-  
-
-
       <div className="flex flex-col items-center text-center md:text-left space-y-2">
         <p className="text-lg whitespace-nowrap">Правовая информация</p>
       </div>
 
       <div className="flex justify-center w-full mt-4">
         <div className="flex items-center justify-center space-x-2">
-          <img src="public/gap.svg" alt="Gap Icon" className="h-6" />
+          <img src="/gap.svg" alt="Gap Icon" className="h-6" />
           <p>г. Москва, Ленинградское шоссе, 13А</p>
         </div>
       </div>
-  
 
       <div className="flex justify-center md:justify-start">
-        <img className="logo h-12" src="public/logo.svg" alt="Laf Logo" />
+        <img className="logo h-12" src="/logo.svg" alt="Laf Logo" />
       </div>
-
+    </div>
   </div>
-</div>
 </footer>
   );
 }
